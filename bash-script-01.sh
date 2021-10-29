0fed179cbbf785b79865b4c16c64f5bff302c7d0
DanielQFK <DanialQueen2004@gmail.com>
2021-10-29 02:35:29 -0400

    ...

---
+++ bash-script-01.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+#"""it is something to be able to run this file by bash """
+# In bash you can write any command which is related to lunux terminal , for example
+sudo apt update
+sudo ate upgrade 
+# and ...
+# By writting the code and run it in the terminal by "bash (file name)"
+
