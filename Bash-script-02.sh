98726cf44e0cec698687385a685d9f941dbcfe42
DanielQFK <DanialQueen2004@gmail.com>
2021-10-31 23:55:15 -0400

    ...

---
+++ Bash-script-02.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Variables 
+Name="Max" #Tip:all variables and = and values have to be close together without space
+Lastname="Cruise"
+# to print , write echo
+# to call the variable , put $ before the variables name...
+echo His name is $Name and his last name is $Lastname ...
+"""Output: His name is Max and his last name is Cruise ... """
+
+
+Name2="Clara"
+Lastname2="Pixer"
+Age=32
+echo Her name is $Name2 and her last name is $Lastname2 and she is $Age years old now...
+"""Output: Her name is Clara and her last name is Pixer and she is 32 years old now... """
+
+Name="AB"
+Lastname="CD"
+echo Hello everybody I am $Name $Lastname
+
